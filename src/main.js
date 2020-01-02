5e4af76c6b9729fe4e748f9565dc7e888b1db25e
autumnFish <517729329@qq.com>
2020-01-02 14:42:24 +0800

    项目起步 - Element-ui整合

--- src/main.js
+++ src/main.js
@@ -2,6 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 // 导入路由对象
 import router from './router/index.js'
+// 导入 饿了么 ui的库
+import ElementUI from 'element-ui';
+// 导入 饿了么 的css
+import 'element-ui/lib/theme-chalk/index.css';
+
+// 注册 饿了么 ui
+Vue.use(ElementUI);
 
 Vue.config.productionTip = false
 
