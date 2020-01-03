e9130e520e297e47c485309d1c06aae194a12a3a
autumnFish <517729329@qq.com>
2020-01-03 11:57:13 +0800

    登录模块 - 表单区域布局

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -4,11 +4,53 @@
     <div class="left">
       <!-- 标题盒子 -->
       <div class="title-box">
-        <img src="../../assets/logo.png" alt="">
+        <img src="../../assets/logo.png" alt="" />
         <div class="title">黑马面面</div>
         <div class="line"></div>
         <div class="sub-title">用户登录</div>
       </div>
+      <!-- 表单 -->
+      <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="43px" class="demo-ruleForm login-form">
+        <!-- 手机号 -->
+        <el-form-item prop="phone">
+          <el-input class="high-input" prefix-icon="el-icon-user" v-model="ruleForm.phone" placeholder="请输入手机号"></el-input>
+        </el-form-item>
+        <!-- 密码 -->
+        <el-form-item prop="password">
+          <el-input
+            show-password
+            prefix-icon="el-icon-lock"
+            v-model="ruleForm.password"
+            placeholder="请输入密码"
+          ></el-input>
+        </el-form-item>
+        <!-- 验证码 -->
+        <el-form-item prop="code">
+          <el-row>
+            <!-- 验证码 -->
+            <el-col :span="18">
+              <el-input prefix-icon="el-icon-key" v-model="ruleForm.code" placeholder="请输入验证码"></el-input>
+            </el-col>
+            <el-col class="code-col" :span="6">
+              <!-- 一部分 -->
+              <img src="../../assets/code.jpg" alt="" />
+            </el-col>
+          </el-row>
+        </el-form-item>
+        <!-- 协议 -->
+        <el-form-item>
+          <el-checkbox v-model="checked">
+            我已阅读并同意
+            <el-link type="primary">用户协议</el-link>
+            和
+            <el-link type="primary">隐私条款</el-link>
+          </el-checkbox>
+        </el-form-item>
+        <el-form-item>
+          <el-button class="login-btn" type="primary" @click="submitForm('ruleForm')">立即创建</el-button>
+          <el-button class="login-btn reset-btn" type="primary" @click="resetForm('ruleForm')">重置</el-button>
+        </el-form-item>
+      </el-form>
     </div>
     <!-- 右边的图片 -->
     <img class="bg" src="../../assets/login_banner_ele.png" alt="" />
@@ -16,8 +58,61 @@
 </template>
 
 <script>
+// 定义验证手机号的方法
+const validatePhone = (rule, value, callback) => {
+  if (value === "") {
+    callback(new Error("手机号不能为空"));
+  } else {
+    // 定义正则 正则  对象
+    const reg = /^(0|86|17951)?(13[0-9]|15[012356789]|166|17[3678]|18[0-9]|14[57])[0-9]{8}$/;
+    // 验证
+    if (reg.test(value) == true) {
+      // 对
+      callback();
+    } else {
+      // 错
+      callback(new Error("小老弟，手机号，写错了哟 O(∩_∩)O哈哈~"));
+    }
+  }
+};
 export default {
-  name: "login"
+  name: "login",
+  data() {
+    return {
+      ruleForm: {
+        phone: "",
+        password: "",
+        code: ""
+      },
+      rules: {
+        phone: [{ validator: validatePhone, trigger: "blur" }],
+        password: [
+          { required: true, message: "密码不能为空", trigger: "blur" },
+          { min: 6, max: 20, message: "长度在 6 到 20 个字符", trigger: "change" }
+        ],
+        code: [
+          { required: true, message: "验证码", trigger: "blur" },
+          { min: 4, max: 4, message: "长度在必须为4", trigger: "change" }
+        ]
+      }
+    };
+  },
+   methods: {
+      submitForm(formName) {
+        // 等同于 this.$refs.ruleForm
+        this.$refs[formName].validate((valid) => {
+          if (valid) {
+            this.$message.success("验证成功")
+          } else {
+            this.$message.error("格式不对哦，检查一下呗！")
+            return false;
+          }
+        });
+      },
+      resetForm(formName) {
+        this.$refs[formName].resetFields();
+      }
+    }
 };
 </script>
 
@@ -39,32 +134,57 @@ export default {
     // 盒模型 让padding不会撑开盒子
     box-sizing: border-box;
     // 标题盒子
-    .title-box{
+    .title-box {
       display: flex;
       // 上下居中
       align-items: center;
       margin-left: 48px;
-      img{
+      img {
         margin-right: 16px;
         width: 22px;
         height: 17px;
       }
-      .title{
-        font-size:24px;
+      .title {
+        font-size: 24px;
         font-weight: 400;
         margin-right: 14px;
       }
-      .line{
+      .line {
         width: 1px;
         background-color: #dfdfdf;
         margin-right: 12px;
         height: 28px;
       }
-      .sub-title{
+      .sub-title {
         font-size: 22px;
         font-weight: 400;
-
       }
+    }
+    // 登录表单
+    .login-form{
+      padding-right: 41px;
+      margin-top: 27px;
+      // 栅格 验证码
+      .code-col {
+        height: 40px;
+        img{
+          width: 100%;
+          height: 100%;
+        }
+      }
+      // 更高的文本框
+      .high-input >input{
+        height: 45px;
+        line-height: 45px
+      }
+    }
+    // 表单内部的 按钮
+    .login-btn {
+      width: 100%;
+      margin-left: 0;
+    }
+    .reset-btn{
+      margin-top: 28px;
     }
   }
   .bg {
