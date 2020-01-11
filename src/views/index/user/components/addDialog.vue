9f86ca0ead212d834fb70261b137a09b0b2bb0d5
autumnFish <517729329@qq.com>
2020-01-11 10:16:59 +0800

    用户模块 - 抽取邮箱 手机号验证函数

--- src/views/index/user/components/addDialog.vue
+++ src/views/index/user/components/addDialog.vue
@@ -11,14 +11,14 @@
       <el-form-item label="电话" :label-width="formLabelWidth" prop="phone">
         <el-input v-model="addForm.phone" autocomplete="off"></el-input>
       </el-form-item>
-      <el-form-item label="角色" prop="role_id" :label-width="formLabelWidth" >
+      <el-form-item label="角色" prop="role_id" :label-width="formLabelWidth">
         <el-select class="normal" v-model="addForm.role_id" placeholder="请选择角色">
           <el-option label="管理员" value="2"></el-option>
           <el-option label="老师" value="3"></el-option>
           <el-option label="学生" value="4"></el-option>
         </el-select>
       </el-form-item>
-      <el-form-item label="状态" prop="status" :label-width="formLabelWidth" >
+      <el-form-item label="状态" prop="status" :label-width="formLabelWidth">
         <el-select class="normal" v-model="addForm.status" placeholder="请选择状态">
           <el-option label="启用" value="1"></el-option>
           <el-option label="禁用" value="0"></el-option>
@@ -39,6 +39,9 @@
 // import { subjectAdd } from "../../../../api/subject.js";
 // @等同于 /src
 import { userAdd } from "@/api/user.js";
+
+// 导入 验证的方法
+import { validatePhone, validateEmail } from "@/utils/validator.js";
 
 export default {
   methods: {
@@ -89,23 +92,37 @@ export default {
       },
       // 表单的验证规则
       addRules: {
-        rid: [
+        username: [
           {
             required: true,
-            message: "学科编号，不能为空",
+            message: "用户名，不能为空",
             trigger: "change"
           }
         ],
-        name: [
+        email: [
           {
             required: true,
-            message: "学科名称，不能为空",
+            validator: validateEmail,
+            trigger: "change"
+          }
+        ],
+        phone: [
+          {
+            required: true,
+            validator: validatePhone,
+            trigger: "change"
+          }
+        ],
+        role_id: [
+          {
+            required: true,
+            message: "用户角色，不能为空",
             trigger: "change"
           }
         ]
       }
     };
-  },
+  }
   // created() {
   //   // window.console.log(subjectAdd);
   // }
